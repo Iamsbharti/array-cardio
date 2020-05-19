fa4a66851c0fb6b4c26f06f75cbce623d740e7ad
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-05-19 10:54:32 +0530

    array.prototype.some
    array.prototype.some

--- src/index.js
+++ src/index.js
@@ -132,3 +132,24 @@ const instances = data.reduce((obj, item) => {
   return obj;
 }, {});
 console.log(instances);
+
+//Array Cardio 2
+const people_v2 = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 }
+];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 }
+];
+
+//Array.prototype.some
+//find is someone is older than 19 years
+const isAdult = people_v2.some(p => new Date().getFullYear() - p.year > 19);
+console.log({ isAdult });
